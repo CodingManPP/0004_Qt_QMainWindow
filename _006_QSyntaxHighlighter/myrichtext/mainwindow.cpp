213143b339f5b7437b3c840f14280003f7f4cc58
wsxingjun <1185889308@qq.com>
2018-11-20 16:37:36 +0800

    HTML支持高亮

--- _006_QSyntaxHighlighter/myrichtext/mainwindow.cpp
+++ _006_QSyntaxHighlighter/myrichtext/mainwindow.cpp
@@ -11,6 +11,9 @@ MainWindow::MainWindow(QWidget *parent) :
     /* 编辑器文档指针做参数，每当编辑器中的文本改变时都会调用highlightBlock函数设置语法高亮*/
     highlighter = new MySyntaxHighlighter(ui->textEdit->document());
 
+    /*使用html高亮*/
+    ui->textEdit->append(tr("<h1><font color=red>使用HTML</font></h1>"));
+
 }
 
 MainWindow::~MainWindow()
